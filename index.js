98975737b47909abf343aba5d93b9c66af544973
Patrick Hennessey <pjhennessey89@gmail.com>
2024-08-03 11:14:34 -0500

    Added text color question and added it to the svg image

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const questions = [
         name: 'text',
         message: 'Enter up to three characters for you logo:',
         validate: (answer) => answer.length > 3 ? 'Must be three characters or less.' : true
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Enter a color (a keyword or hexadecimal number) for the text color:'
     }
 ]
 
@@ -14,14 +19,15 @@ async function createLogo() {
     try {
         const answers = await inquirer.prompt(questions);
         const text = answers.text
+        const color = answers.color
         const svg = `
         <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-            <text x="10" y="40" font-family="Arial" font-size="40" fill="black">${text}</text>
+            <text x="10" y="40" font-family="Arial" font-size="40" fill="${color}">${text}</text>
         </svg>
         `;
 
         fs.writeFileSync('logo.svg', svg);
-        
+
         console.log('Logo saved');
     } catch (error) {
         console.error(error);
